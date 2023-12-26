d48b4d37280260a593aba4e687d5246c3d283d8a
Kirill <minecrta@yandex.ru>
2023-12-26 17:36:49 +0300

    add first version

---
+++ git_test/git_test.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class People
+{
+public:
+	People(string name, int age) : name(name), age(age) {};
+
+	const void print() { cout << "Name: " << name << "\n" << "Age: " << age; }
+
+private:
+	string name;
+	int age;
+};
+
+int sum(int a, int b) {
+	return a + b;
+}
+
+int main()
+{
+	People vlad = { "vlad",12 };
+	vlad.print();
+	cout << endl << sum(10, 6);
+}
